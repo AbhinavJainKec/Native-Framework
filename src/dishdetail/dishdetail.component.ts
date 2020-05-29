04ef5e933f9d632dc89b5f946e519f1ae2dbb9e9
Abhinav Jain <abhinav.jain@krishnacollege.ac.in>
2020-05-30 00:53:26 +0530

    Buttons

--- src/dishdetail/dishdetail.component.ts
+++ src/dishdetail/dishdetail.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute, Params } from "@angular/router";
 import { Location } from "@angular/common";
 import { RouterExtensions } from 'nativescript-angular/router';
 import { switchMap } from 'rxjs/operators';
+import { FavoriteService } from "../services/favorite.service";
 
 @Component({
   selector: 'app-dishdetail',
@@ -18,22 +19,42 @@ export class DishdetailComponent implements OnInit {
     dish: Dish;
     comment: Comment;
     errMess: string;
+    avgstars: string;
+    numcomments: number;
+    favorite: boolean = false;
 
     constructor(
         private dishservice: DishService,
         private route: ActivatedRoute,
         private routerExtensions: RouterExtensions,
+        private favoriteservice: FavoriteService,
         @Inject('baseURL') private baseURL) { }
     
     ngOnInit() {
     
         this.route.params
-          .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-          .subscribe(dish => this.dish = dish,
-              errmess => { this.dish = null; this.errMess = <any>errmess; });
-    }
+        .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+        .subscribe(dish => { 
+            this.dish = dish;
+            this.favorite = this.favoriteservice.isFavorite(this.dish.id);
+            this.numcomments = this.dish.comments.length;
+
+            let total = 0;
+            this.dish.comments.forEach(comment => total += comment.rating);
+            this.avgstars = (total/this.numcomments).toFixed(2);
+            },
+            errmess => { this.dish = null; this.errMess = <any>errmess; });
+  }
+
     
     goBack(): void {
         this.routerExtensions.back();
     }
+
+    addToFavorites() {
+        if (!this.favorite) {
+          console.log('Adding to Favorites', this.dish.id);
+          this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+        }
+      }
 }
