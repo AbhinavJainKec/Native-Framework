1da5427b3faf692c859888f98d0b67dbae670459
Abhinav Jain <abhinav.jain@krishnacollege.ac.in>
2020-05-30 23:08:09 +0530

    Application Settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import * as app from "tns-core-modules/application"
 import { RouterExtensions } from "nativescript-angular/router";
 import { DrawerTransitionBase, RadSideDrawer, SlideInOnTopTransition } from "nativescript-ui-sidedrawer";
 import { filter } from "rxjs/operators";
+import { login, LoginResult } from "tns-core-modules/ui/dialogs";
+import { getString, setString } from "tns-core-modules/application-settings";
 
 @Component({
     selector: "ns-app",
@@ -46,5 +48,24 @@ export class AppComponent implements OnInit {
 
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.closeDrawer();
+    }
+
+    displayLoginDialog() {
+        let options = {
+            title: "Login",
+            message: 'Type Your Login Credentials',
+            userName: getString("userName", ""),
+            password: getString("password",""),
+            okButtonText: "Login",
+            cancelButtonText: "Cancel"
+        }
+
+        login(options)
+            .then((loginResult: LoginResult) => {
+                setString("userName", loginResult.userName);
+                setString("password", loginResult.password);
+            },
+            () => { console.log('Login cancelled'); 
+        });
     }
 }
